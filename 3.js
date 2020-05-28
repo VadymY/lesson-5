a840e17f50983c0c11c0b5be319a6d34b53ba9d7
Vadym Yashchenko <vadym.yashchenko@gmail.com>
2020-05-28 13:27:07 +0300

    1

---
+++ 3.js
@@ -0,0 +1,32 @@
+/**
+ * Задача 3.
+ *
+ * Создайте функцию `f`, которая отнимает от первого параметра второй и делит на третий.
+ *
+ * Условия:
+ * - Генерировать ошибку, если в качестве любого входного аргумента было предано не число.
+ */
+
+// Решение
+
+function f(a, b, c){
+    let ar = [a, b, c];
+    for  (let item of ar)
+    {
+        if ( typeof(item) !== "number")
+        {
+            throw new Error("not a number");
+        }
+    }
+
+    if (c === 0)
+    {
+        throw new Error("third argument must be not null");
+    }
+
+    return (a - b) / c;
+}
+
+console.log(f(9, 3, 2)); // 3
+
+exports.f = f;
---
+++ 8.js
@@ -0,0 +1,48 @@
+/**
+ * Задача 7.
+ *
+ * Создайте функцию `f`, которая принимает массив в качестве параметра.
+ * Функция возвращает undefined, и последовательно выводит в консоль (с помощью console.log) элементы массива,
+ * переданного в качестве аргумента.
+ *
+ * Условия:
+ * - Генерировать ошибку, если в качестве входного аргумента был передан не массив;
+ * - Генерировать ошибку, если в качестве входного аргумента был передан пустой массив;
+ * - Обязательно использовать рекурсию;
+ * - Использовать циклы запрещено.
+ *
+ * Заметки:
+ * - Возможно вам понадобится метод splice → https://developer.mozilla.org/uk/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+ */
+
+// Решение
+
+function f (...args)
+{
+    if (typeof(args) !== "object" || !args[0])
+    {
+        throw new Error("not an array");
+    }
+
+    if (args[0].length == 0)
+    {
+        throw new Error("array must be not empty");
+    }
+
+    if (args[0].length === 1)
+    {
+        return console.log(args[0][0]);
+    }
+
+   let temp = args[0].splice(0, args[0].length - 1);
+   f(temp);
+    return console.log(args[0][0]);
+}
+
+f([1, 2, 3]);
+
+// 1
+// 2
+// 3
+
+exports.f = f;
